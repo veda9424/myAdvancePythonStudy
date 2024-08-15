89f0f8d6ed2c1fa5d8fab34685c4a4a10729bef8
Vedant Ravindra Divekar <vedantravindradivekar@Vedants-MacBook-Air.local>
2024-08-15 17:14:02 +0530

    script added

---
+++ Javascript/basic.js
@@ -0,0 +1,17 @@
+console.log("Run JavaScript");
+console.log(1+2);
+//var varName = value;
+var bankAccount = 100;
+bankAccount
+100
+var deposit = 50;
+var total = bankAccount 
+var total = bankAccount + deposit
+total
+150
+var greeting = "Welcome back: "; 
+var names = "Jose";
+alert(greeting + names);
+var myVariable
+var bonus = null;
+alert("hey")
---
+++ Javascript/myscript.js
@@ -0,0 +1,4 @@
+alert("Wlecome to youe Bank!");
+var deposit = prompt("How much you would like to deposit today? ")
+alert("You've deposited: "+deposit)
+console.log("You are a cool person!")
